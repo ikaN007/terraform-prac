5d734eebaf3040c47a90c8cbafaa1de1da0c1b40
ikaN007 <50532187+ikaN007@users.noreply.github.com>
2022-02-09 02:00:20 +0530

    Update aws.tf

--- ec2prov/aws.tf
+++ ec2prov/aws.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  access_key = "AKIAVS5YJVOWYIMTG7FP"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
   region     = "ap-south-1"
 }
